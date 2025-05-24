bf9436c59ffdff65708b5dca8e740b7e42937359
imchristianlowe <imchristianlowe@gmail.com>
2025-05-24 06:55:19 -0400

    update

--- init.lua
+++ init.lua
@@ -222,7 +222,7 @@ require('lazy').setup({
     config = function()
       require('project_nvim').setup{
         detection_methods = { "pattern", "lsp" },
-        patterns = { ".git", "src", "docker-compose.yml" },
+        patterns = { ".git", "src", "docker-compose.yml", "package.json" },
       }
     end
   },
@@ -271,6 +271,36 @@ require('lazy').setup({
       autochdir = true
     }},
   },
+  {
+      "ThePrimeagen/harpoon",
+      branch = "harpoon2",
+      dependencies = { "nvim-lua/plenary.nvim" }
+  },
+{
+  "linux-cultist/venv-selector.nvim",
+    dependencies = {
+      "neovim/nvim-lspconfig",
+      "mfussenegger/nvim-dap", "mfussenegger/nvim-dap-python", --optional
+      { "nvim-telescope/telescope.nvim", branch = "0.1.x", dependencies = { "nvim-lua/plenary.nvim" } },
+    },
+  lazy = false,
+  branch = "regexp", -- This is the regexp branch, use this for the new version
+  config = function()
+      require("venv-selector").setup({
+      -- settings = {
+      --   search = {
+      --     my_venvs = {
+      --       command = "fd python$ ~/code/lifesync/backend"
+            -- }
+      --     }
+      --   }
+      }
+    )
+    end,
+    keys = {
+      { ",v", "<cmd>VenvSelect<cr>" },
+    },
+},
 }, {})
 
 -- [[ Setting options ]]
@@ -652,7 +682,43 @@ cmp.setup {
   },
 }
 
+-- Harpoon Setup
+local harpoon = require("harpoon")
 
+-- REQUIRED
+harpoon:setup()
+-- REQUIRED
+
+-- basic telescope configuration
+local conf = require("telescope.config").values
+local function toggle_telescope(harpoon_files)
+    local file_paths = {}
+    for _, item in ipairs(harpoon_files.items) do
+        table.insert(file_paths, item.value)
+    end
+
+    require("telescope.pickers").new({}, {
+        prompt_title = "Harpoon",
+        finder = require("telescope.finders").new_table({
+            results = file_paths,
+        }),
+        previewer = conf.file_previewer({}),
+        sorter = conf.generic_sorter({}),
+    }):find()
+end
+
+vim.keymap.set("n", "<leader>b", function() harpoon:list():add() end, {desc = "Add buffer to harpoon"})
+vim.keymap.set("n", "<C-e>", function() toggle_telescope(harpoon:list()) end,
+    { desc = "Open harpoon window" })
+
+-- vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
+-- vim.keymap.set("n", "<C-t>", function() harpoon:list():select(2) end)
+-- vim.keymap.set("n", "<C-n>", function() harpoon:list():select(3) end)
+-- vim.keymap.set("n", "<C-s>", function() harpoon:list():select(4) end)
+--
+-- Toggle previous & next buffers stored within Harpoon list
+vim.keymap.set("n", "<C-p>", function() harpoon:list():prev() end)
+vim.keymap.set("n", "<C-n>", function() harpoon:list():next() end)
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
 --#vim.opt.shell = '/bin/zsh\ -i'
